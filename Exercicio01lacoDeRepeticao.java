e1340601f2b468345ee4a58107d32de0514da8d6
PabloCotrim <pablocotrimbarbosa1499@gmail.com>
2022-01-27 23:27:25 -0300

    JavaProjeto

---
+++ Exercicio01lacoDeRepeticao.java
@@ -0,0 +1,20 @@
+package br.com.generation.Exercicios;
+
+public class Exercicio01lacoDeRepeticao {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+
+		for(int i=1000; i<2000; i++) {
+			if(i %11 == 5) {
+				System.out.println("Valores dividos por 11 com o resto 5: "+i);
+				Thread.sleep(500);
+	}
+	
+		
+	}
+	
+	
+	}
+
+}
