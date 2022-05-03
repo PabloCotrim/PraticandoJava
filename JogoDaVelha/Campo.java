eaa496aadf6d498267276f9de5843863da6be7d9
PabloCotrim <pablocotrimbarbosa1499@gmail.com>
2022-05-03 17:16:59 -0300

    Jogo da Velha

---
+++ JogoDaVelha/Campo.java
@@ -0,0 +1,18 @@
+package praticandoExercicios;
+
+public class Campo {
+	private char simbolo;
+	public Campo() {
+		this.simbolo = ' ';
+	}
+	public char getSimbolo() {
+		return this.simbolo;
+	}
+	public void setSimbolo(char simbolo) {
+		if(this.simbolo ==' ') {
+			this.simbolo = simbolo;
+		}else {
+			System.out.println("campo em uso");
+		}
+	}
+}
